6ad0e967f6ad4eff12856bfbd819c422b2db7101
Mr.Song <852227791@qq.com>
2018-10-24 21:10:31 +0800

    add mas user login  page

--- Platform/Presentation/UserCenter/Controllers/HomeController.cs
+++ Platform/Presentation/UserCenter/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace UserCenter.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            
 
             return View();
         }
